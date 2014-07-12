f76a67059b3ce7128d23788f83304f6a3e9f6aaa
Ran <abbshr@outlook.com>
2014-07-12 12:38:24 +0800

    fix bug in onOnce, freeze the core event _proto

--- event.js
+++ event.js
@@ -15,38 +15,56 @@
   if (!global.CustomEvent)
     throw new Error('your browser doesnt support custom events');
 
-  var _proto = global.document.createElement('i');
+  //var _proto = global.document.createElement('i');
 
   var EventEmitter = function (options) {
-    this.MAXListener = options.MAXListener || 100;
-    this.config = options.config || { bubbles: false, cancelable: false };
-    //this._event = {};
+    this.MAXListener = options ? (options.MAXListener || 100) : 100;
+    this.config = options ? 
+      (options.config || { bubbles: false, cancelable: false }) : 
+      { bubbles: false, cancelable: false };
+    
+    // make the _proto property private and readonly
+    Object.defineProperty(this, '_proto', {
+      value: global.document.createElement('i'),
+      writeable: false,
+      enumerable: false,
+      configurable: false
+    });
+
+    // freeze the core event object
+    Object.freeze(this._proto);
   };
 
   EventEmitter.prototype.emit = function (eName, data) {
+    var _self = this;
     var settings = {};
-    for (var i in this.config)
-      if (this.config.hasOwnProperty(i))
-        settings[i] = this.config[i];
+    for (var i in _self.config)
+      if (_self.config.hasOwnProperty(i))
+        settings[i] = _self.config[i];
     settings.detail = data;
+
+    // create a new event with above settings
     var event = new CustomEvent(eName, settings);
-    _proto.dispatchEvent(event);
+    _self._proto.dispatchEvent(event);
   };
 
   EventEmitter.prototype.on = function (eName, cb) {
-    _proto.addEventListener(eName, cb, false);
+    var _self = this;
+    _self._proto.addEventListener(eName, cb, false);
   };
 
   EventEmitter.prototype.onOnce = function (eName, cb) {
+    var _self = this;
     var handler = function (e) {
       cb(e);
-      _proto.removeListener(eName, handler);
+      _self._proto.removeEventListener(eName, handler);
     };
-    _proto.addEventListener(eName, handler, false);
+    _self._proto.addEventListener(eName, handler, false);
   };
 
   EventEmitter.prototype.removeListener = function (eName, cb) {
-    _proto.removeListener(eName, cb);
+    var _self = this;
+    _self._proto.removeEventListener(eName, cb);
   };
 
   global.EventEmitter = EventEmitter;
